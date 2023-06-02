9568ce286daab31943a5ded981150e13ceda6e8c
polarzero <0xpolarzero@gmail.com>
2023-06-02 16:36:30 +0200

    Init shadcn ui next template

---
+++ frontend/lib/fonts.ts
@@ -0,0 +1,14 @@
+import {
+  Source_Code_Pro as FontMono,
+  Inter as FontSans,
+} from "next/font/google"
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const fontMono = FontMono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
