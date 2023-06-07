00c63942f0511eeb5f71e00402350c62176c6bfd
polarzero <0xpolarzero@gmail.com>
2023-06-07 12:05:36 +0200

    Update form, add checkbox

--- frontend/components/submit-project-form.tsx
+++ frontend/components/submit-project-form.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 import useGlobalStore from "@/stores/useGlobalStore"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { waitForTransaction } from "@wagmi/core"
-import { Loader2 } from "lucide-react"
+import { Loader2, LucideAsterisk } from "lucide-react"
 import { useForm } from "react-hook-form"
 import { useContractWrite } from "wagmi"
 import * as z from "zod"
@@ -25,6 +25,12 @@ import {
   FormMessage,
 } from "@/components/ui-custom/form"
 
+import { Checkbox } from "./ui/checkbox"
+
+/* -------------------------------------------------------------------------- */
+/*                                   SCHEMA                                   */
+/* -------------------------------------------------------------------------- */
+
 const isValidUrl = (url: string) => {
   try {
     new URL(url)
@@ -40,17 +46,16 @@ const formSchema = z
     projectName: z.string().min(2).max(50),
     // Description is optional
     projectDescription: z.string().optional(),
-    // Links are optional, but should be string of valid urls separated by commas
+    // Links are optional, but should be an array of strings of valid urls
     projectLinks: z
-      .string()
+      .array(z.string())
       .optional()
       .refine((val) => {
         if (!val) return true
-        const urls = val.split(",")
-        return urls.every(isValidUrl)
+        return val.every(isValidUrl)
       }),
-    // Tags are optional
-    projectTags: z.string().optional(),
+    // Tags are optional, but should be an array of strings
+    projectTags: z.array(z.string()).optional(),
     // Collaborators should be at least 1 address long
     projectCollaborators: z.array(z.string()).min(1),
     // Same for shares with a total of 100
@@ -58,6 +63,11 @@ const formSchema = z
       .array(z.number())
       .min(1)
       .refine((val) => val.reduce((a, b) => a + b, 0) === 100),
+    // Must accept terms and conditions
+    conditions: z
+      .boolean()
+      .default(false)
+      .refine((val) => val === true),
   })
   // Collaborators & shares should be the same length
   .refine(
@@ -67,6 +77,16 @@ const formSchema = z
       path: ["projectCollaborators", "projectShares"],
     }
   )
+
+/* -------------------------------------------------------------------------- */
+/*                                    FORM                                    */
+/* -------------------------------------------------------------------------- */
+
+const Required = () => (
+  <span className="absolute ml-2 mt-0.5">
+    <LucideAsterisk size={10} color="var(--light-red)" />
+  </span>
+)
 
 export function SubmitProjectForm() {
   const { address, currentNetwork } = useGlobalStore((state) => ({
@@ -80,10 +100,11 @@ export function SubmitProjectForm() {
     defaultValues: {
       projectName: "",
       projectDescription: "",
-      projectLinks: "",
-      projectTags: "",
+      projectLinks: [],
+      projectTags: [],
       projectCollaborators: [address],
       projectShares: [],
+      conditions: false,
     },
   })
 
@@ -95,6 +116,8 @@ export function SubmitProjectForm() {
 
   function onFormSubmit(values: z.infer<typeof formSchema>) {
     console.log(values)
+    // Transform links & tags to string
+    // submit
   }
 
   const { isLoading: isSubmittingProject, write: submitProject } =
@@ -163,22 +186,96 @@ export function SubmitProjectForm() {
     <div className="w-full">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onFormSubmit)} className="space-y-8">
+          {/* -------------------------------------------------------------------------- */
+          /*                                 projectName                                */
+          /* -------------------------------------------------------------------------- */}
           <FormField
             control={form.control}
             name="projectName"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
+                <FormLabel>
+                  Name <Required />
+                </FormLabel>
                 <FormControl>
-                  <Input placeholder="Project's name" {...field} />
+                  <Input placeholder="The name of the project" {...field} />
                 </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
+          {/* -------------------------------------------------------------------------- */
+          /*                             projectDescription                             */
+          /* -------------------------------------------------------------------------- */}
+          <FormField
+            control={form.control}
+            name="projectDescription"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="A short description of the project"
+                    {...field}
+                  />
+                </FormControl>
+                {/* <FormDescription>
+                  This is the name of the project.
+                </FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* -------------------------------------------------------------------------- */
+          /*                                projectLinks                                */
+          /* -------------------------------------------------------------------------- */}
+          <FormField
+            control={form.control}
+            name="projectLinks"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Links</FormLabel>
+                <FormControl>
+                  {/* Can add multiple inputs for strings */}
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* ADD CHECKBOX 'You understand that EVERYTHING is stored onchain... will keep a transparent track of the project and its contributions' */}
+          {/* -------------------------------------------------------------------------- */
+          /*                                 conditions                                 */
+          /* -------------------------------------------------------------------------- */}
+          <FormField
+            control={form.control}
+            name="conditions"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-center space-x-3 space-y-0 rounded-md border p-4">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <div className="space-y-1 leading-none">
+                  <FormLabel>
+                    You understand that{" "}
+                    <span className="underline">everything</span> on this
+                    platform is stored on chain.
+                  </FormLabel>
+                  <FormDescription>
+                    All records of the project, including the contributions,
+                    will be publicly and transparently available.
+                  </FormDescription>
+                </div>
+              </FormItem>
+            )}
+          />
+
+          {/* -------------------------------------------------------------------------- */
+          /*                                   FOOTER                                   */
+          /* -------------------------------------------------------------------------- */}
           <div
             className={`flex grow items-center ${
               isProcessingTransaction ? "justify-between" : "justify-end"
