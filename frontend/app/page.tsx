91031e0ce9d9cfe7b74428eea431a85b7b08f14f
polarzero <0xpolarzero@gmail.com>
2023-06-09 23:20:26 +0200

    Update description

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,15 +8,14 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Your decentralized crowdfunding hub.
+          Your decentralized crowdfunding platform.
           <br className="hidden sm:inline" />
           Enabling automated and flexible project support
           <br className="hidden sm:inline" />
-          through blockchain technology.
+          through blockchain and Chainlink.
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-          Cascade is a crowdfunding platform with a difference. It enables a new
-          level of{" "}
+          Cascade is a crowdfunding platform that tries to enable a new level of{" "}
           <b>control and flexibility in supporting projects you believe in</b>.
           Leveraging <b>Chainlink Automation</b>, Cascade ensures{" "}
           <b>precise, periodic payments</b> to chosen projects, embodying the
@@ -24,6 +23,11 @@ export default function IndexPage() {
           bank deductions, you dedicate an amount of your choice from your
           contributor account, for each project. These funds are then
           automatically distributed at intervals you specify.
+          <br className="hidden sm:inline" />
+          <br className="hidden sm:inline" />
+          As the member of a project, you can stay confident that each
+          collaborator is being paid their <b>fair share</b>,{" "}
+          <b>without having to worry about the logistics</b> of the process.
           <br className="hidden sm:inline" />
           <br className="hidden sm:inline" />
           <b>
