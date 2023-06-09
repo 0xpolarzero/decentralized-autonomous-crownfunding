0b8805b775f6ba139bc3582cdc222deffebd34b1
polarzero <0xpolarzero@gmail.com>
2023-06-09 21:10:16 +0200

    Add loading

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -1,3 +1,9 @@
+import { Loader2 } from "lucide-react"
+
 export default function Loading() {
-  return <div>TEST LOADING</div>
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Loader2 className="w-10 h-10 opacity-50 animate-spin" />
+    </div>
+  )
 }
